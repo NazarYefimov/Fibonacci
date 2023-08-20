d0386f71c19cad930eb12c2df4bd0a6ceff59bb3
Цезарь <nexushonor29@gmail.com>
2023-08-20 20:03:17 +0300

    1

---
+++ src/main/java/org/example/IterativeFibonacci.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class IterativeFibonacci {
+    public static int iterativeFibonacci(int n) {
+        if (n <= 1)
+            return n;
+
+        int prev = 0;
+        int curr = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+
+        return curr;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int result = iterativeFibonacci(n);
+        System.out.println("Fibonacci(" + n + ") = " + result);
+    }
+}
---
+++ src/main/java/org/example/RecursiveFibonacci.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class RecursiveFibonacci {
+    public static int recursiveFibonacci(int n) {
+        if (n <= 1)
+            return n;
+
+        return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int result = recursiveFibonacci(n);
+        System.out.println("Fibonacci(" + n + ") = " + result);
+    }
+}
